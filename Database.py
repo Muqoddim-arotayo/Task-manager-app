b61467708cdd04d8e08c6938844cba7d1c0181ed
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-22 23:31:45 +0100

    added a task manager file

---
+++ Database.py
@@ -0,0 +1,12 @@
+import pymysql as pyms
+import os   
+from dotenv import load_dotenv
+load_dotenv()
+
+db_password = os.getenv("DB_PASS")
+
+my_Con = pyms.connect(host='127.0.0.1', user='root', passwd=db_password, db="TaskManager")
+my_Cursor = my_Con.cursor()
+ 
+
+
---
+++ Task_manager.py
@@ -0,0 +1,24 @@
+from Database import my_Con
+from Database import my_Cursor
+
+class TaskManager :
+    def __init__(self):
+        pass
+
+    def add_task():
+        pass
+    
+    def view_task():
+        pass
+
+    def update_task():
+        pass
+
+    def delete_task():
+        pass
+
+    def mark_as_complete():
+        pass
+
+    def set_priority():
+        pass
