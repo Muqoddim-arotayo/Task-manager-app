df72c8327a9d18f3d9b96d7b416021931fd70c87
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-09-04 23:08:20 +0100

    deleted the former inbuilt database and addded the mysql database configuration to the database.py file

--- Database.py
+++ Database.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()  
 
-db_password = os.getenv("DB_PASS")
+db_password = os.getenv("DB_PASS")  
 
 my_Con = pyms.connect(host='127.0.0.1', user='root', passwd=db_password, db="TaskManager")
 my_Cursor = my_Con.cursor()
